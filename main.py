a1743874d1eef3c955e678fb971e29a9cf3aa5f5
Saheer Anas <saheer09anas@gmail.com>
2023-09-25 00:02:14 +0530

    change ws to wss in client code address

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://fastapi-socket-example.onrender.com/ws");
+            var ws = new WebSocket("wss://fastapi-socket-example.onrender.com/ws");
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
